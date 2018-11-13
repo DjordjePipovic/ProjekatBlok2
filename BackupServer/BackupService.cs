215678041eb5e0353e50895eefed0d74865c7187
Djordje Pipovic <dinosavur@gmail.com>
2018-11-13 22:08:07 +0100

    Projekat

---
+++ BackupServer/BackupService.cs
@@ -0,0 +1,64 @@
+﻿using Contracts;
+using Interfejsi;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace BackupServer
+{
+    public class BackupService : IBackupService
+    {
+        public void BackupLog(Dictionary<int, MyEvent> dic)
+        {
+            string s = "";
+
+            foreach(KeyValuePair<int, MyEvent> kvp in dic)
+            {
+                s += kvp.Key.ToString() + " " + kvp.Value.ToString() + Environment.NewLine;
+            }
+
+            File.WriteAllText("BackupServer.txt", s);
+            Console.WriteLine("BackupServer/BackupLog()");
+        }
+
+        public Dictionary<int, MyEvent> LoadLog()
+        {
+            Dictionary<int, MyEvent> ret = new Dictionary<int, MyEvent>();
+            string[] split = File.ReadAllLines("BackupServer.txt");
+
+            foreach(string s in split)
+            {
+                ret.Add(konverzija(s).Key, konverzija(s).Value);
+            }
+
+            Console.WriteLine("BackupServer/LoadLog()");
+            return ret;
+        }
+
+        public KeyValuePair<int, MyEvent> konverzija(string s)
+        {
+            string[] s1 = s.Split(';');
+
+            int id = Int32.Parse(s.Split(' ')[0]);
+            string c = s1[0].Split('=')[1];
+            DateTime dt = Convert.ToDateTime(s1[1].Split('=')[1]);
+            string sc = s1[2].Split('=')[1];
+            string m = s1[3].Split('=')[1];
+            Enum.TryParse(s1[4].Split('=')[1], out EnumState es);
+
+            MyEvent me = new MyEvent(c, dt, sc, m, es);
+
+            return new KeyValuePair<int, MyEvent>(id, me);
+        }
+
+        public void TestCommunication()
+        {
+            Console.WriteLine("Komunikacija sa Backup Server-om je uspostavljena.");
+        }
+    }
+}
---
+++ BackupServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using Contracts;
+using Interfejsi;
+using Sertifikati;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Policy;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// srvCertCN.SubjectName should be set to the service's username. .NET WindowsIdentity class provides information about Windows user running the given process
+			string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;            
+
+            string address = "net.tcp://localhost:9003/BackupService";
+            ServiceHost host = new ServiceHost(typeof(BackupService));
+            host.AddServiceEndpoint(typeof(IBackupService), binding, address);
+
+            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+            
+            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+            host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+
+            try
+            {
+                host.Open();
+                Console.WriteLine("BackupServer is started.\nPress <enter> to stop ...");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
+            }
+        }
+    }
+}
